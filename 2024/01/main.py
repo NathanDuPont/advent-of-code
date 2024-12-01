dd8a6e55e65c8712f037880c2944705a57ac2172
NathanDuPont <nathan.dupont01@gmail.com>
2024-11-30 21:27:48 -0800

    2024.1

---
+++ 2024/01/main.py
@@ -0,0 +1,34 @@
+import math
+import sys
+import copy
+
+def solve() -> int:
+    sys.setrecursionlimit(200000)
+    file = open('input.txt', 'r')
+    read = file.readlines()
+    stripped = map(lambda s : s.replace("\n", ""), read)
+
+    left = []
+    right = []
+    right_occurrences = {}
+    for i, l in enumerate(stripped):
+        a, b = l.split("   ")
+        left.append(int(a))
+        right.append(int(b))
+
+        right_occurrences[int(b)] = right_occurrences.get(int(b), 0) + 1
+
+    sorted_left = sorted(left)
+    sorted_right = sorted(right)
+
+    total = 0
+    similarity = 0
+    for i, num in enumerate(sorted_left):
+        total += abs(sorted_left[i] - sorted_right[i])
+        similarity += num * right_occurrences.get(num, 0)
+
+    print(f"Difference: {total}")
+    print(f"Similarity: {similarity}")
+
+if __name__ == '__main__':
+    solve()
---
+++ common/template.py
@@ -0,0 +1,14 @@
+import math
+import sys
+import copy
+
+def solve() -> int:
+    sys.setrecursionlimit(200000)
+    file = open('input.txt', 'r')
+    read = file.readlines()
+    stripped = map(lambda s : s.replace("\n", ""), read)
+    for i, l in enumerate(stripped):
+        pass
+
+if __name__ == 'main':
+    print(solve())
